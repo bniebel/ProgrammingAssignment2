a3a4355ffd25f4d8af567fff21042eee1dc80db9
Benjamin J. Niebel <bniebel@gmail.com>
2014-12-14 18:25:06 -0500

    Beginning Work
    I’ve just started working on Assignment #2.  I’m practicing a commit.

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ In this example we introduce the `<<-` operator which can be used to
 assign a value to an object in an environment that is different from the
 current environment. Below are two functions that are used to create a
 special object that stores a numeric vector and caches its mean.
-
+/Users/bniebel/Documents/ProgrammingAssignment2/README.md
 The first function, `makeVector` creates a special "vector", which is
 really a list containing a function to
 
--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## The makeCacheMatrix function creates a list that contains functions
+##1. to set the value of the vector [makeCacheMatrix()], 
+##2. get the value of the vector [get()],
+##3. set the value of the mean, and
+##4. get the value of the mean
+makeCacheMatrix <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(x) m <<- -x
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getmean()
+  m<- -x
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
 }
